c39919b7b8922035916c28809a51e2aefbebde17
growni <sa6ko19961126@gmail.com>
2025-04-14 19:26:34 +0300

    Debugging token expire date

--- Backend/src/main/java/com/example/Backend/services/Impl/AuthService.java
+++ Backend/src/main/java/com/example/Backend/services/Impl/AuthService.java
@@ -122,6 +122,10 @@ public class AuthService {
 
         PasswordResetToken resetToken = tokenFromDb.get();
 
+        System.out.println("Is token expired: " + resetToken.isExpired());
+        System.out.println("Token expires at: " + resetToken.getExpirationDate());
+        System.out.println("Now: " + Instant.now());
+
         if(resetToken.isExpired()) {
             return false;
         }
